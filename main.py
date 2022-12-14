2b2adb759e928e2ebd8af5e6b6e400309a008f73
saar2666 <saar2666@gmail.com>
2022-12-14 15:42:37 +0200

    5

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import time
+from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
 import json
+from pydantic import BaseModel
 
 app = FastAPI()
 db = []
@@ -29,23 +31,9 @@ def get_currency_by_id(currency_id: int):
     return db[currency_id - 1]
 
 
-@app.get('/currency')
+@app.get ('/currency/')
 def get_all_currency():
-    api_call = requests.get(
-        'https://v6.exchangerate-api.com/v6/786cc711780443f0973ae997/latest/USD').json()  # no spaces, http and not https
-    get_rates = api_call['rates']
-    currency_list = list(k for k, v in get_rates.items() if v < 10)
-    return json.dumps(currency_list, indent=4)
-
     return db
-
-def dev_exchange(): #this function generate json file (one time only) with the api data
-    currency_names = []
-    api_call = requests.get('https://v6.exchangerate-api.com/v6/786cc711780443f0973ae997/latest/USD').json()
-    with open('all_currency_file_new.json', 'w') as outfile:
-        json.dump(api_call, outfile, indent=4)
-
-#dev_exchange()
 
 
 @app.delete('/currency/{currency_id}')
